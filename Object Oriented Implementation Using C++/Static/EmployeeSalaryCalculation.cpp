6d772af5cc894898e3de22b163e7d4b257f53af1
syalabi-seet <syalabi.seet@hotmail.com>
2023-06-15 21:13:09 +0800

    stuck at static

--- Object Oriented Implementation Using C++/Static/EmployeeSalaryCalculation.cpp
+++ Object Oriented Implementation Using C++/Static/EmployeeSalaryCalculation.cpp
@@ -1,7 +1,69 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
+class Employee{
+    private:
+        int count;
+        int employeeId;
+        string employeeName;
+        double salary;
+        double netSalary;
+    
+    public:
+        void setEmployeeId(int id){
+            employeeId = id;
+        }
+        int getEmployeeId(){
+            return employeeId;
+        }
+        void setEmployeeName(string name){
+            this->employeeName = name;
+        }
+        string getEmployeeName(){
+            return this->employeeName;
+        }
+        void setSalary(double sal){
+            this->salary = sal;
+        }
+        double getSalary(){
+            return this->salary;
+        }
+        void setNetSalary(double netSal){
+            this->netSalary = netSal;
+        }
+        double getNetSalary(){
+            return this->netSalary;
+        }
+        void calculateNetSalary(int pfpercentage){
+            netSalary = salary * (1 - (pfpercentage/100));
+        }
+        void display(){
+            cout << "Id: " << employeeId << endl;
+            cout << "Name: " << employeeName << endl;
+            cout << "Net Salary (In dollars): " << netSalary << endl;
+        }
+        void getEmployeeDetails(){
+            cout << "Enter Name:" << endl;
+            cin >> employeeName;
+            cout << "Enter salary (In dollars):" << endl;
+            cin >> salary;            
+        }
+        static int generateEmployeeId(int id){
+            
+        }
+};
+
 int main(){
+    //create object for Employee class
+    Employee employee;
+    employee.getEmployeeDetails();
+    
+    int pfpercentage;
+    cout<<"Enter PF percentage:"<<endl;
+    cin>>pfpercentage;
+
+    int Employee::generateEmployeeId(employee.);
     
     return 0;
 }
--- Object Oriented Implementation Using C++/Static/StudentGradeCalculator.cpp
+++ Object Oriented Implementation Using C++/Static/StudentGradeCalculator.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 using namespace std;
-class College
-{
+
+class College{
     private:
-        string principal_name;  // principal_name is common for all the students
+        static string principal_name;  // principal_name is common for all the students
     public:
-        static void setPrincipalName(string name)
-        {
+        static void setPrincipalName(string name){
             principal_name=name;
         }
-       static string getPrincipalName()
-        {
+       static string getPrincipalName(){
             return principal_name;
         }
 };
+
 //Initialize the static principal_name variable with value "John" here
+string College::principal_name = "John";
+
 class Student
 {
     private:
@@ -25,71 +26,78 @@ class Student
         char grade;
         College cObj;
     public:
-        void getStudentDetails();
-        void calculateAverage();
-        void findGrade();
-        void displayStudentDetails();
-        void setId(int id)
-        {
+        void getStudentDetails(){
+            cout << "Enter Student ID :" << endl;
+            cin >> id;
+            cout << "Enter Student Name :" << endl;
+            cin >> name;
+
+            cout << "Enter five subject marks :" << endl;
+            for (int i = 0; i < 5; i++){
+                cin >> marks[i];
+            }    
+        }
+        void calculateAverage(){
+            float totalMarks{0};
+            for (float mark: marks){
+                totalMarks += mark;
+            }
+            average = totalMarks / 5;
+        }
+        void findGrade(){
+            if (average >= 80 && average <= 100) grade = 'O';
+            else if (average < 50) grade = 'F';
+            else grade = 'A';
+        }
+        void displayStudentDetails(){
+            cout << "Student Name : " << name << endl;
+            cout << "Student ID : " << id << endl;
+            cout << "Principal Name : " << cObj.getPrincipalName() << endl;
+            cout << "Average : " << getAverage() << endl;
+            cout << "Grade : " << getGrade() << endl;
+        }
+        void setId(int id){
             this->id=id;
         }
-        int getId()
-        {
+        int getId(){
             return id;
         }
-        void setAverage(float average)
-        {
+        void setAverage(float average){
             this->average=average;
         }
-        int getAverage()
-        {
+        int getAverage(){
             return average;
         }
-        void setName(string n)
-        {
+        void setName(string n){
             this->name=n;
         }
-        string getName()
-        {
+        string getName(){
             return name;
         }
-        void setMarks(float m[])
-        {
-            for(int i=0; i<5;i++)
-                this->marks[i]=m[i];
+        void setMarks(float m[]){
+            for (int i = 0; i < 5; i++){
+                marks[i] = m[i];
+            }
         }
-        float* getMarks()
-        {
+        float* getMarks(){
             return marks;
         }
-        void setGrade(char g)
-        {
+        void setGrade(char g){
             grade=g;
         }
-        char getGrade()
-        {
+        char getGrade(){
             return grade;
         }
-        College getCollegeObject()
-        {
+        College getCollegeObject(){
             return cObj;
         }
 };
-int main()  //DO NOT change the 'main' signature
-{
-    //Fill code here
-    int id;
-    string name;
-    int marks[5];
-    cout << "Enter Student ID :" << endl;
-    cin >> id;
-    cout << "Enter Student Name :" << endl;
-    cin >> name;
 
-    cout << "Enter five subject marks :" << endl;
-    for (int i = 0; i < 5; i++){
-        cin >> marks[i];
-    }
-    
+
+int main(){
+    //Fill code here
+    Student student;
+    student.getStudentDetails();
+    student.displayStudentDetails();   
     return 0;
 }
